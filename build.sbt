15970aee141ca0e0ae259211ca8ad59331318413
levi <info@velocorner.com>
2017-06-26 21:19:54 +0200

    add pickling support

--- build.sbt
+++ build.sbt
@@ -11,8 +11,10 @@ scalacOptions ++= List("-target:jvm-1.6", "-feature", "-deprecation", "-language
 
 net.virtualvoid.sbt.graph.Plugin.graphSettings
 
-libraryDependencies += "org.scala-lang.modules" %% "spores-core" % "0.1.3"
+libraryDependencies += "ch.epfl.scala" %% "spores" % "0.4.3"
 
-libraryDependencies += "org.scala-lang.modules" %% "spores-pickling" % "0.1.3"
+libraryDependencies += "org.scala-lang.modules" %% "scala-pickling" % "0.10.1"
+
+addCompilerPlugin("ch.epfl.scala" %% "spores-serialization" % "0.4.3")
 
 
--- src/main/scala/spores/Main.scala
+++ src/main/scala/spores/Main.scala
@@ -1,15 +1,18 @@
 package spores
 
 import scala.spores._
+import scala.pickling.Defaults._, scala.pickling.json._
 
 /**
- * Created by levi on 16/06/15.
+ * http://docs.scala-lang.org/sips/pending/spores.html
  */
 object Main extends App {
 
   case class Person(name: String, age: Int)
   val outer1 = 0
   val outer2 = Person("Jim", 35)
+
+  println("spore inner and outer classes")
 
   val s = spore {
     val factor = 1
@@ -20,4 +23,7 @@ object Main extends App {
     }
   }
 
+  //val pkl = s.pickle
+  //val person = pkl.unpickle[Person]
+  //println(s"unpickled $person")
 }
